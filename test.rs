2de4d8ca14b468b7bddf3eab104433a01066de23
Owen Stenson <stensonowen@gmail.com>
2016-11-28 03:20:54 -0500

    Add Rust and C demos for testing
    This has posed a bit of an issue. Writing the Rust function the proper
    way (i.e. not relying on bools/ints being the same thing) breaks the
    pattern we're using to look for branches that depend on specific
    function calls. We can try to make the pattern checking generic, we
    could tailor it to the Rust IR product, or we can alter the Rust source
    to produce IR that looks like C.

---
+++ test.rs
@@ -0,0 +1,28 @@
+//Test for basic __builtin_expect_ usage
+
+//Compile without stdlib: don't have to link to test
+//Pretty sure to compile a *binary* w/o stdlib you need nightly Rust
+#![no_std]
+#![no_main]
+#![feature(lang_items, start)]
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {} }
+
+//#[no_mangle]
+// generics apparently require mangling
+//	we can mangle a generic function or no_mangle a (bool,bool)
+#[inline]
+pub fn __builtin_expect_<T: Eq>(expr: T, expect: T) -> i32 {
+	(expr == expect) as i32
+}
+
+#[no_mangle] 
+pub extern fn main(argc: i32, _argv: *const *const u8) -> i32 {
+	if __builtin_expect_(argc, 2) == 1 {
+		1
+	} else {
+		0
+	}
+}
+
+
